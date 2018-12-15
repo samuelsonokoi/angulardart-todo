e571eb08c8b9f110d4f45464699f3f8111f8126c
Samuelson Okoi <samuelsonokoi@gmail.com>
2018-12-15 14:48:09 +0100

    modified todo service

--- lib/src/todo_list/todo_list_service.dart
+++ lib/src/todo_list/todo_list_service.dart
@@ -5,7 +5,20 @@ import 'package:angular/core.dart';
 /// Mock service emulating access to a to-do list stored on a server.
 @Injectable()
 class TodoListService {
-  List<String> mockTodoList = <String>[];
+  List<String> mockTodoList = thingsTodo().toList();
+  // List<String> mockTodoList = <String>[];
 
   Future<List<String>> getTodoList() async => mockTodoList;
+}
+
+Iterable<String> thingsTodo() sync* {
+  var actions = ['Walk', 'Wash', 'Feed'];
+  var pets = ['cats', 'dogs'];
+
+  for (var action in actions) {
+    for (var pet in pets) {
+      if (pet == 'cats' && action != 'Feed') continue;
+      yield '$action the $pet';
+    }
+  }
 }
